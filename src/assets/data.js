355361fd151755e41a5d230808c18c9a8ec5b132
sumayy-ansar <your.email@example.com>
2025-02-10 08:49:35 +0530

    Set up identity and fix line endings

---
+++ src/assets/data.js
@@ -0,0 +1,98 @@
+export const data=[
+{
+    question:"If the base sequence in DNA is 5' AAAT 3' then the base sequence in mRNA is",
+    option1:"3'UUUU5'",
+    option2:"5'AAAU3'",
+    option3:"3'AAAU5'",
+    option4:"5'UUUU3'",
+    ans:2,
+},
+{
+
+question:"Avery, MacLeod and Mc Carty used the S and R strains of streptococcus pneumoniae. They isolated and purified protein, DNA, RNA from the bacteria and treated them with related enzymes. They concluded that ",
+
+option1:"DNA was transforming agent",
+option2:"RNA was transforming agent",
+option3:"Protein was transforming agent",
+option4:"All are correct",
+ans:1,
+},
+{
+    question:"Identify the characteristic which is not applicable to the genetic code",
+    option1:"Non-Polar",
+    option2:"Non-Overlapping",
+    option3:"Commaless",
+    option4:"universal",
+    ans:1,
+
+},
+{
+    question:"Ribose is differentiable from deoxyribose in having ",
+    option1:"Two extra oxygen",
+    option2:"No oxygen",
+    option3:"Hydroxyl group at 2nd carbon",
+    option4:"universalOne extra hydrogen",
+    ans:3,
+
+},
+{
+    question:"A DNA strand is directly involved in the synthesis of all the following except ",
+    option1:"Another DNA",
+    option2:"t-RNA & m-RNA",
+    option3:"r-RNA",
+    option4:"Protein",
+    ans:4,
+
+},
+{
+    question:"The genes are responsible for growth and differentiation in an organism through regulation of  ",
+    option1:"Translocation",
+    option2:"Transformation",
+    option3:"Transduction and translation",
+    option4:"Translation and transcription",
+    ans:4,
+
+},
+{
+    question:"Genetic information is carried out by long chain molecule made up of  ",
+    option1:"Amino acids",
+    option2:"Enzymes",
+    option3:"Nucleotides",
+    option4:"Histone proteins",
+    ans:3,
+
+},
+{
+    question:"Anticodons are found in  ",
+    option1:"mRNA",
+    option2:"tRNA",
+    option3:"rRNA",
+    option4:"In all",
+    ans:2,
+
+},
+{
+    question:"Which of the following element is not found in nitrogenous base  ",
+    option1:"Nitrogen",
+    option2:"Hydrogen",
+    option3:"Carbon",
+    option4:"Phosphorus",
+    ans:4,
+
+},
+{
+    question:"Transfer of genetic information from a polymer of nucleotides to a polymer of amino acids is ",
+    option1:"Replication",
+    option2:"Transcription",
+    option3:"Translation",
+    option4:"Reverse transcription",
+    ans:3,
+
+},
+
+
+
+
+
+
+];
